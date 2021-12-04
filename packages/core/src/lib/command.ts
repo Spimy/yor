97563018da4d6f399deb9e150d4482db6f21074c
Spimy <officialspyme@gmail.com>
2021-12-04 22:14:10 +0400

    Add base command and command decorator

---
+++ packages/core/src/lib/command.ts
@@ -0,0 +1,27 @@
+import { Store } from './store';
+import { Message, PermissionResolvable } from 'discord.js';
+
+export interface CommandInfo {
+  name: string;
+  aliases?: string[];
+  description?: string;
+  permissions?: PermissionResolvable[];
+}
+
+export interface CommandData extends CommandInfo {
+  execute?: (message: Message) => Promise<void>;
+}
+
+export abstract class BaseCommand {
+  constructor(private info: CommandInfo) {}
+
+  abstract execute(message: Message): Promise<void>;
+
+  public getInfo(): CommandInfo {
+    return { ...this.info };
+  }
+}
+
+export function Command() {
+  return (target: { new (): BaseCommand }) => Store.addCommand(target);
+}
