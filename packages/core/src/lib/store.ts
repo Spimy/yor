8e1fdf5f1a53e78be85fac2c052d594a6ec97aec
Spimy <officialspyme@gmail.com>
2021-12-05 00:18:26 +0400

    Add prefix to command store

--- packages/core/src/lib/store.ts
+++ packages/core/src/lib/store.ts
@@ -2,6 +2,7 @@ import Collection from '@discordjs/collection';
 import { CommandData, BaseCommand } from './command';
 
 class CommandStore {
+  private prefix: string = '!';
   private commands: Collection<string, CommandData> = new Collection();
   private aliases: Collection<string, string> = new Collection();
 
@@ -11,6 +12,14 @@ class CommandStore {
 
   get $aliases() {
     return this.aliases;
+  }
+
+  get $prefix() {
+    return this.prefix;
+  }
+
+  set $prefix(prefix: string) {
+    this.prefix = prefix;
   }
 
   public addCommand(target: { new (): BaseCommand }) {
