f480d4e31f325a05f849a9c224d0c81c4cf1186c
Spimy <officialspyme@gmail.com>
2021-12-05 00:18:58 +0400

    Use prefix from store

--- packages/commands/src/index.ts
+++ packages/commands/src/index.ts
@@ -1,4 +1,4 @@
-import { BaseModule, MuseClient, Store } from '@muse/core';
+import { BaseModule, MuseClient, Store, ModuleWithArgument } from '@muse/core';
 import { Message } from 'discord.js';
 
 export type CommandType = 'classic' | 'slash';
@@ -11,23 +11,30 @@ export interface CommandExecutorOptions {
 export class CommandExecutor extends BaseModule {
   constructor(client: MuseClient, { type, prefix }: CommandExecutorOptions) {
     super(client);
+    Store.$prefix = prefix || Store.$prefix;
 
     if (type === 'classic') {
       client.on('messageCreate', async (message: Message) => {
         if (message.author.bot) return;
         if (message.channel.type === 'DM') return;
-        if (!message.content.startsWith(prefix || '!')) return;
+        if (!message.content.startsWith(Store.$prefix)) return;
 
         const cmd = message.content.slice(1);
+        const args = message.content
+          .slice(Store.$prefix.length + cmd.length)
+          .split(' ');
         const command = Store.getCommand(cmd.toLowerCase());
         if (command) {
-          command.execute(message);
+          command.execute(message, args);
         }
       });
     }
   }
 
-  static create(options: CommandExecutorOptions) {
-    return BaseModule.initialiseWithArguments([options]);
+  static create(options: CommandExecutorOptions): ModuleWithArgument {
+    return {
+      module: this,
+      args: [options],
+    };
   }
 }
