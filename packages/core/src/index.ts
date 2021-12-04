2bd363b6625fd871f975ae3c4fb9d887272b5a36
Spimy <officialspyme@gmail.com>
2021-12-04 22:15:35 +0400

    Export everything from core package

---
+++ packages/core/src/index.ts
@@ -0,0 +1,9 @@
+export { MuseClient, MuseClientModules, MuseClientOptions } from './client';
+export { Store } from './lib/store';
+export {
+  BaseModule,
+  ModuleWithArgument,
+  Module,
+  ModuleConstructor,
+} from './lib/module';
+export { Command, CommandInfo, CommandData, BaseCommand } from './lib/command';
