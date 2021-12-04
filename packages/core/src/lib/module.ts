03d52a6087a09d9847afb8bf9d7e499b377514a1
Spimy <officialspyme@gmail.com>
2021-12-04 22:15:16 +0400

    Add extendable base module

---
+++ packages/core/src/lib/module.ts
@@ -0,0 +1,23 @@
+import { MuseClient } from '../client';
+
+export interface ModuleWithArgument {
+  module: typeof BaseModule;
+  args: any[];
+}
+
+export type Module = ModuleWithArgument | typeof BaseModule;
+
+export interface ModuleConstructor {
+  new (client: MuseClient, args?: any[]): BaseModule;
+}
+
+export abstract class BaseModule {
+  constructor(protected client: MuseClient) {}
+
+  static initialiseWithArguments(args: any[]): ModuleWithArgument {
+    return {
+      module: this,
+      args,
+    };
+  }
+}
