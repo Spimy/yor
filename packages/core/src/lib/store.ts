14dc8ab161a9ad2ed2ff6f768c7ae370a8d057ae
Spimy <officialspyme@gmail.com>
2021-12-04 22:13:23 +0400

    Add store for commands and aliases

---
+++ packages/core/src/lib/store.ts
@@ -0,0 +1,35 @@
+import Collection from '@discordjs/collection';
+import { CommandData, BaseCommand } from './command';
+
+class CommandStore {
+  private commands: Collection<string, CommandData> = new Collection();
+  private aliases: Collection<string, string> = new Collection();
+
+  public getCommands() {
+    return this.commands;
+  }
+
+  public getAliases() {
+    return this.aliases;
+  }
+
+  public addCommand(target: { new (): BaseCommand }) {
+    const command = new target();
+    const commandInfo = command.getInfo();
+
+    this.commands.set(command.getInfo().name, {
+      ...commandInfo,
+      execute: command.execute,
+    });
+
+    commandInfo.aliases?.forEach((alias) => {
+      this.aliases.set(alias, commandInfo.name);
+    });
+  }
+
+  public getCommand(name: string) {
+    return this.commands.get(name) || this.commands.get(this.aliases.get(name));
+  }
+}
+
+export const Store = new CommandStore();
