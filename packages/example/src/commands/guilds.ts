f0cc3e7c7daf7ae4c06e8172ca5306ad8ac0bb7e
Spimy <officialspyme@gmail.com>
2022-04-18 15:35:07 +0400

    Add guilds command

---
+++ packages/example/src/commands/guilds.ts
@@ -0,0 +1,21 @@
+import { Command, BaseCommand, Store } from '@muse/core';
+import { Message } from 'discord.js';
+
+@Command()
+export default class extends BaseCommand {
+  constructor() {
+    super({
+      name: 'guilds',
+      aliases: ['g'],
+      description: 'View guilds that the bot is in'
+    });
+  }
+
+  async execute(message: Message) {
+    message.channel.send(
+      Store.$client.guilds.cache
+        .map((guild) => `**${guild.name}**: ${guild.id}`)
+        .join('\n')
+    );
+  }
+}
