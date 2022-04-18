89ff3ebba8aac82812c669d05fc960c6ceb9db42
Spimy <officialspyme@gmail.com>
2022-04-18 15:39:24 +0400

    Refactor to use event handler instead

--- packages/commands/src/index.ts
+++ packages/commands/src/index.ts
@@ -1,7 +1,13 @@
-import { BaseModule, MuseClient, Store, ModuleWithArgument } from '@muse/core';
+import {
+  BaseModule,
+  Store,
+  ModuleWithArgument,
+  Event,
+  BaseEvent
+} from '@muse/core';
 import { Message } from 'discord.js';
 
-export type CommandType = 'classic' | 'slash';
+export type CommandType = 'classic';
 
 export interface CommandExecutorOptions {
   type: CommandType;
@@ -9,32 +15,35 @@ export interface CommandExecutorOptions {
 }
 
 export class CommandExecutor extends BaseModule {
-  constructor(client: MuseClient, { type, prefix }: CommandExecutorOptions) {
-    super(client);
+  constructor({ type, prefix }: CommandExecutorOptions) {
+    super();
     Store.$prefix = prefix || Store.$prefix;
 
     if (type === 'classic') {
-      client.on('messageCreate', async (message: Message) => {
-        if (message.author.bot) return;
-        if (message.channel.type === 'DM') return;
-        if (!message.content.startsWith(Store.$prefix)) return;
+      @Event('messageCreate')
+      class ReadyEvent extends BaseEvent {
+        async execute(message: Message): Promise<void> {
+          if (message.author.bot) return;
+          if (message.channel.type === 'DM') return;
+          if (!message.content.startsWith(Store.$prefix)) return;
 
-        const cmd = message.content.slice(1);
-        const args = message.content
-          .slice(Store.$prefix.length + cmd.length)
-          .split(' ');
-        const command = Store.getCommand(cmd.toLowerCase());
-        if (command) {
-          command.execute(message, args);
+          const cmd = message.content.slice(1);
+          const args = message.content
+            .slice(Store.$prefix.length + cmd.length)
+            .split(' ');
+          const command = Store.getCommand(cmd.toLowerCase());
+          await command?.execute(message, args);
+          return;
         }
-      });
+      }
+      return;
     }
   }
 
   static create(options: CommandExecutorOptions): ModuleWithArgument {
     return {
       module: this,
-      args: [options],
+      args: [options]
     };
   }
 }
