79033b3b6f0271c3e9a398fde55ff1b487e25c38
Spimy <officialspyme@gmail.com>
2022-04-18 15:37:08 +0400

    Add event handler decorator

---
+++ packages/core/src/lib/handlers/event.ts
@@ -0,0 +1,14 @@
+import { ClientEvents } from 'discord.js';
+import { EventEmitter } from 'stream';
+import { Store } from '../store';
+
+export abstract class BaseEvent {
+  abstract execute(...args: any[]): Promise<void>;
+}
+
+export function Event(event: keyof ClientEvents) {
+  return (target: { new (): BaseEvent }) => {
+    if (Store.$client.$debug) console.log(`Loaded event: ${event}`);
+    Store.$client.on(event, new target().execute);
+  };
+}
